b3319eb78dfc7b5d8d89fadf2febfcdb33be6511
Padam Sethia <padamsethia5@gmail.com>
2017-08-01 00:49:25 +0530

    Updated Config with dummy values

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ SECRET_KEY = 'the_very_secure_secret_security_key_that_no_will_ever_guess'
 # MySQL Config
 
 
-host = "shorturl-db.cluster-cdhgmi7rbrso.ap-south-1.rds.amazonaws.com"
-user = "mksrtuimaster"
-passwrd = "Pix3aiG4#eic$eeho"
-db = "mobishorturl"
+host = "localhost"  
+user = "root"
+passwrd = "pass"
+db = "shorty"
 
 # Domain Host
 
@@ -19,7 +19,7 @@ db = "mobishorturl"
 For now , use http as using https returns a bad error message , 
 For https , use a SSL certificate. ( under works)
 '''
-domain = "http://35.154.109.125:5000/"
+domain = "http://localhost:5000/"
 
 # Domain just for display , for r.g https://goo.gl/asdar will be goo.gl/asdar
-domain_disp = "mbkwk.com/"
+domain_disp = "shorty.com/"
